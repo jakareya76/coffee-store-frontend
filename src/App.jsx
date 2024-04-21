739cfbd1aa3a0f8f79091ee3ecfb50d96d7594c5
jakareya76 <jakareya1306@gmail.com>
2024-04-21 20:43:10 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold">hello</h2>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import AddCoffee from "./pages/AddCoffee.jsx";
+import UpdateCoffee from "./pages/UpdateCoffee.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/add-coffee",
+    element: <AddCoffee />,
+  },
+  {
+    path: "/update-coffee",
+    element: <UpdateCoffee />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
